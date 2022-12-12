76fd84caeb1fd0c64cd74ef50c4a3d0bf6749ab0
senad96 <57908168+senad96@users.noreply.github.com>
2022-12-12 13:30:32 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+#The main file used to run experiments
+
+
+
+def run():
+	
+	
+	x=0
+	return x
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    run()
+    
---
+++ script.sh
@@ -0,0 +1,2 @@
+python JEMPP/eval_jempp.py --load_path models/jempp_M10.pt --eval test_clf --dataset cifar_test --model yopo --norm batch
+
