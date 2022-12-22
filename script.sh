62035ff22cf6f6e1dbcd95ddd59bceda405f7035
senad96 <57908168+senad96@users.noreply.github.com>
2022-12-22 09:34:08 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-python /app/Env-jempp2/eval_jempp.py --load_path /app/models_pretrained/jempp_M10.pt --eval test_clf --dataset cifar_test --model yopo --norm batch
+python /app/Env-jempppppppp/eval_jempp.py --load_path /app/models_pretrained/jempp_M10.pt --eval test_clf --dataset cifar_test --model yopo --norm batch
 
